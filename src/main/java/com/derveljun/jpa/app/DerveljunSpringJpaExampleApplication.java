29af4d114a0f5a7e9a464576a0fe59f5e6e49877
EZK-Jasmine <derveljun@tistory.com>
2019-12-22 20:08:05 +0900

    init

---
+++ src/main/java/com/derveljun/jpa/app/DerveljunSpringJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.derveljun.jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerveljunSpringJpaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DerveljunSpringJpaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/derveljun/jpa/DerveljunSpringJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derveljun.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerveljunSpringJpaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
